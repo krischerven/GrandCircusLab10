5cdce1292f3343d1436103d8259df0e22b2d4700
Kris Cherven <krischerven@gmail.com>
2019-07-26 08:49:19 -0400

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,23 @@
+import java.lang.Math;
+
+public class Circle {
+	private double radius;
+	public Circle(double _radius) {
+		radius = _radius;
+	}
+	public double getCircumference() {
+		return (2*Math.PI)*radius; 
+	}
+	public double getArea() {
+		return Math.PI*(radius*radius);
+	}
+	private static String formatNumber(double x) {
+		return Double.toString(Math.round(x*100)/100);
+	}
+	public String getFormattedCircumference() {
+		return "Circumference: " + formatNumber(getCircumference());
+	}
+	public String getFormattedArea() {
+		return "Area: " + formatNumber(getArea());
+	}
+}
---
+++ src/GrandCircusLab10.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class GrandCircusLab10 {
+	
+	private static double getRadius(Scanner sc) {
+		while (true) {
+			if (sc.hasNextDouble()) {
+				return sc.nextDouble();
+			} else {
+				System.err.println("Error: not a valid double.");
+			}
+		}
+	}
+	
+	private static boolean getYesNo(Scanner sc) {
+		while (true) {
+			String line = sc.nextLine().toLowerCase().trim();
+			if (line.equals("y") || line.equals("yes")) {
+				return true;
+			} else if (line.equals("n") || line.equals("no")) {
+				return false;
+			} else {
+				System.out.println("Please input y/n or yes/no.");
+			}
+		}
+	}
+
+	public static void main(String[] args) {	
+		final Scanner sc = new Scanner(System.in);
+		boolean cont = true;
+		int circles = 0;
+		while (cont) {
+			System.out.println("Please input a circle radius.");
+			final Circle circ = new Circle(getRadius(sc));
+			++circles;
+			sc.nextLine(); // discard garbage line
+			System.out.println(circ.getFormattedArea());
+			System.out.println(circ.getFormattedCircumference());
+			System.out.println("Make another circle? (y/n)");
+			cont = getYesNo(sc);
+		}
+		String s = "";
+		if (circles != 1) {
+			s = "s";
+		}
+		System.out.println("Goodbye. You made " + circles + " circle"+s+".");
+	}
+}
